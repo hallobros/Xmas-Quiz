06391207c43ed5e18190e44e18d2881ec9cb6fca
hallobros <67079621+hallobros@users.noreply.github.com>
2020-12-23 20:14:45 +0000

    more questions

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const correctAnswers = ['D','C','B','B','A'];
+const correctAnswers = ['D','C','B','B','A','D','A','A','D','B'];
 
 const form = document.querySelector('.quiz-form')
 const result = document.querySelector('.result')
+
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     let score = 0;
-    const userAnswers = [form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value]
+    const userAnswers = [form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value,form.q6.value,form.q7.value,form.q8.value,form.q9.value,form.q10.value]
 
     userAnswers.forEach((answer, index) => {
         if(answer === correctAnswers[index]){
-            score += 20;
+            score += 10;
         }
     });
 
@@ -27,5 +28,6 @@ form.addEventListener('submit', e => {
         output++
     }
     }, 30);
+
 })
 
